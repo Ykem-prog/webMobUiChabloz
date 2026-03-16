a531d2c00ae398f9a207486dd4fcfc7e215ca208
Ykem-prog <ykem99@gmail.com>
2026-03-16 16:07:14 +0100

    Initialisation of the repo

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+
+import Temperature from './components/Temperature.vue';
+</script>
+
+<template>
+ 
+  <Temperature />
+</template>
---
+++ src/components/Temperature.vue
@@ -0,0 +1,22 @@
+<script>import { ref } from 'vue'
+import { computed } from 'vue'
+
+const temperature = ref(8)
+console.log(temperature.value)
+temperature.value = 10
+console.log(temperature.value)  
+const plusOne = computed(() => temperature.value + 1)
+
+console.log(plusOne.value) 
+
+
+
+</script>
+<template>
+  <div>
+    <h2>Convertisseur Temperature</h2>
+    <p>{{ temperature }}°F</p>
+  </div>
+</template>
+
+<style></style>
